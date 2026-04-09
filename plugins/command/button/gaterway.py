bd4e989ed64e7968894a80d298a72e157e95e5e6
tiloperrs <tiloperrr@gmail.com>
2026-04-09 16:42:54 -0500

    update bot

--- plugins/command/button/gaterway.py
+++ plugins/command/button/gaterway.py
@@ -3,12 +3,12 @@ from paquetes.plantillas import tipogt
 from db.mongo_client import MongoDB
 
 @Client.on_callback_query(filters.regex("gates"))
-def gates_coman(client, message):
-    querY = MongoDB().query_user(int(message.from_user.id))
-    message.edit_message_text('''<b>
+def gates_coman(client, m):
+    querY = MongoDB().query_user(int(m.from_user.id))
+    m.edit_message_text('''<b>
 𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨 𝐍𝐮𝐞𝐬𝐭𝐫𝐚 𝐙𝐨𝐧𝐚 𝐝𝐞 𝐆𝐚𝐭𝐞𝐰𝐚𝐲𝐬 ♛
 ━━━━━━━━━━━━━━━━
 ⨳ 𝐆𝐚𝐭𝐞𝐰𝐚𝐲𝐬 𝐚𝐜𝐭𝐢𝐯𝐨𝐬 (5) ✅
 ⨳ 𝐆𝐚𝐭𝐞𝐰𝐚𝐲𝐬 𝐝𝐞𝐜𝐥𝐢𝐧𝐞𝐝 (0) ❌
 ━━━━━━━━━━━━━━━━
-</b>''',reply_markup=tipogt(message.from_user.id))
+</b>''',reply_markup=tipogt(m.from_user.id))
