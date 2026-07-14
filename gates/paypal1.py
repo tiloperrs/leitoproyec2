85e2f4834fd3d708c6e46b0b0142ebaaabfb9896
tiloperrs <tiloperrr@gmail.com>
2026-07-13 20:47:50 -0500

    primer commit

--- gates/paypal1.py
+++ gates/paypal1.py
@@ -26,13 +26,7 @@ def find_between(data, first, last):
   except ValueError:
     return None  
 
-zipcode = direc['postalCode']
-try:
-    city = direc['city']
-except KeyError:
-    city = 'NY'
-state = direc['state']
-street = direc['address1']
+
 
 class paypal1:
     def __init__(self, tarjeta):
